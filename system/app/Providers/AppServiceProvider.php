f634b2fec43180ef823c2a212d252ce31fb19fe1
Maukepoyahhh <kulgarfreefire2022@gmail.com>
2026-04-29 19:46:34 +0700

    Add files via upload

---
+++ system/app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use View;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $config = \DB::table('setings')->where('id',1)->first();
+        
+        View::share('config',$config);
+    }
+}
